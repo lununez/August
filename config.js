53f05224291ac6ac930268ab7290f815c374d17d
lununez <lununez@me.com>
2016-02-17 22:50:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,6 @@ var config = {};
 config.offlineKey = "0123456789abcdef0123456789abcdef";
 config.offlineKeyOffset = 1;
 
-config.sslKey = __dirname+"/ssl-key.pem";
-config.sslCert = __dirname+"/ssl-cert.pem";
-
 config.alexaSkillGatewayURL = "/august/alexa";
 config.baseURL = "/august/control";
 config.httpsServerPort = 8443; //comment if no https server required (https used by alexa skill)
